b113dada2944376c886297a163a482b7d0aaf535
unknown <anna.ruzimova@iis.utm.md>
2024-09-09 12:43:56 +0300

    save new brach

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 import CustomButton from "./CustomButton";
 const Navbar = () => {
   return (
-    <header className="w-full absolute z-101">
+    <header className="w-full z-101">
       <nav className=" max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
         <Link href="/" className="flex justify -center items-center">
           <Image
             src="/logo.svg"
             alt=" Car Hub Logo"
-            width={1180}
+            width={180}
             height={18}
             className="object-contain"
           />
@@ -18,7 +18,7 @@ const Navbar = () => {
         <CustomButton
           title="Sign In"
           btnType="button"
-          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+          containerStyles="text-primary-blue bg-white rounded-full border border-primary-blue px-4 py-2 min-w-[130px] shadow-lg"
         />
       </nav>
     </header>
